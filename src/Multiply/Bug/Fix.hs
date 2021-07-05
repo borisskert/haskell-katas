5d069591f484f16e0cfe3358b0a248199bfd32c6
Boris Skert <boris.skert@gmail.com>
2021-07-05 19:16:50 +0200

    Solve 'Multiply' kata

---
+++ src/Multiply/Bug/Fix.hs
@@ -0,0 +1,4 @@
+module Multiply.Bug.Fix (multiply) where
+
+multiply :: Int -> Int -> Int
+multiply a b = a * b
---
+++ test/Multiply/Bug/FixSpec.hs
@@ -0,0 +1,3 @@
+module Multiply.Bug.FixSpec where
+
+import qualified Multiply.Bug.Fix as SUT
