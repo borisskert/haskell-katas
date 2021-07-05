3679ff81c165ead6b685f593ce822a53a1c511f8
Boris Skert <boris.skert@gmail.com>
2021-07-05 21:35:55 +0200

    Solve 'Sum of positive' kata

---
+++ src/Codewars/Arrays.hs
@@ -0,0 +1,10 @@
+module Codewars.Arrays where
+
+-- https://www.codewars.com/kata/5715eaedb436cf5606000381/train/haskell
+
+positiveSum :: [Int] -> Int
+positiveSum [] = 0
+positiveSum (x:rest) = do
+  if x > 0
+    then x + positiveSum rest
+    else positiveSum rest
---
+++ test/Codewars/ArraysSpec.hs
@@ -0,0 +1,18 @@
+module Codewars.ArraysSpec where
+
+import Codewars.Arrays (positiveSum)
+import Test.Hspec
+
+spec :: Spec
+spec = do
+  describe "positiveSum" $ do
+    it "works for some examples" $ do
+      positiveSum [1, 2, 3, 4, 5] `shouldBe` 15
+      positiveSum [1, -2, 3, 4, 5] `shouldBe` 13
+      positiveSum [-1, 2, 3, 4, -5] `shouldBe` 9
+    
+    it "returns 0 if array is empty" $ do
+      positiveSum [] `shouldBe` 0
+    
+    it "returns 0 if all elements are negative" $ do
+      positiveSum [-1, -2, -3, -4, -5] `shouldBe` 0
