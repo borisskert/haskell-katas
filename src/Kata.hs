24cb93c0cfe36561b94dfb26615149b74c01c737
Boris Skert <boris.skert@gmail.com>
2021-07-06 19:27:04 +0200

    Solve 'Remove String Spaces' kata

---
+++ src/Kata.hs
@@ -0,0 +1,10 @@
+module Kata (noSpace) where
+
+-- https://www.codewars.com/kata/57eae20f5500ad98e50002c5/train/haskell
+
+noSpace :: String -> String
+noSpace "" = ""
+noSpace (c:cs) = do
+  if c == ' '
+    then noSpace cs
+    else c : noSpace cs
---
+++ test/KataSpec.hs
@@ -0,0 +1,13 @@
+module KataSpec where
+import Kata (noSpace)
+import Test.Hspec
+
+spec :: Spec
+spec = do
+  describe "Example Tests" $
+    it "should work with example tests" $ do
+      noSpace "8 j 8   mBliB8g  imjB8B8  jl  B" `shouldBe` "8j8mBliB8gimjB8B8jlB"
+      noSpace "8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd" `shouldBe` "88Bifk8hB8BB8BBBB888chl8BhBfd" 
+      noSpace "8aaaaa dddd r     " `shouldBe` "8aaaaaddddr" 
+      noSpace "jfBm  gk lf8hg  88lbe8 " `shouldBe` "jfBmgklf8hg88lbe8" 
+      noSpace "8j aam" `shouldBe` "8jaam"
