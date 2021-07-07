9a84182aaf286a9ddbf7597e95d416f2e8d7fdaf
Boris Skert <boris.skert@gmail.com>
2021-07-07 20:22:12 +0200

    Solve 'Count of positives / sum of negatives' kata

---
+++ src/Kata.hs
@@ -0,0 +1,9 @@
+module Kata where
+
+-- https://www.codewars.com/kata/576bb71bbbcf0951d5000044/train/haskell
+
+countPositivesSumNegatives :: Maybe [Int] -> [Int]
+countPositivesSumNegatives maybeArray = case maybeArray of
+  Just []    -> []
+  Just array -> [length (filter (>0) array)] ++ [sum (filter (<0) array)]
+  Nothing    -> []
---
+++ test/KataSpec.hs
@@ -0,0 +1,10 @@
+module KataSpec where
+import Test.Hspec
+import Kata (countPositivesSumNegatives)
+
+spec :: Spec
+spec = do
+  describe "Example Test" $
+    it "should work with example tests" $ do
+      countPositivesSumNegatives (Just [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15]) `shouldBe` [10, -65]
+      countPositivesSumNegatives (Just [0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14]) `shouldBe` [8, -50]
