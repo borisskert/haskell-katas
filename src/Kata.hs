9bd861d214c376a526b68f96075a177e2049d5e8
Boris Skert <boris.skert@gmail.com>
2021-07-07 18:40:48 +0200

    Solve 'Is n divisible by x and y' kata

---
+++ src/Kata.hs
@@ -0,0 +1,8 @@
+module Kata (isDivisible) where
+
+isDivisible :: Int -> Int -> Int -> Bool
+isDivisible n x y = do
+  let nDivX = n `div` x
+  let nDivY = n `div` y
+  
+  n == nDivX * x && n == nDivY * y
---
+++ test/KataSpec.hs
@@ -0,0 +1,18 @@
+module KataSpec (spec) where
+
+import Test.Hspec
+import Kata (isDivisible)
+
+spec :: Spec
+spec = do
+  it "Basic tests" $ do
+    isDivisible 3 3 4 `shouldBe` False
+    isDivisible 12 3 4 `shouldBe` True
+    isDivisible 8 3 4 `shouldBe` False
+    isDivisible 48 3 4 `shouldBe` True
+    isDivisible 100 5 10 `shouldBe` True
+    isDivisible 100 5 3 `shouldBe` False
+    isDivisible 4 4 2 `shouldBe` True
+    isDivisible 5 2 3 `shouldBe` False
+    isDivisible 17 17 17 `shouldBe` True
+    isDivisible 17 1 17 `shouldBe` True
