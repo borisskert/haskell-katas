5e7e3e63e09b023ff5bd7f9f0122683bbdcc4c13
Boris Skert <boris.skert@gmail.com>
2021-07-07 20:30:17 +0200

    Solve 'Invert values' kata

---
+++ src/Kata.hs
@@ -0,0 +1,6 @@
+module Kata (invert) where
+
+-- https://www.codewars.com/kata/5899dc03bc95b1bf1b0000ad/train/haskell
+
+invert :: [Integer] -> [Integer]
+invert = map negate
---
+++ test/KataSpec.hs
@@ -0,0 +1,11 @@
+module KataSpec where
+import Kata (invert)
+import Test.Hspec
+
+spec :: Spec
+spec = do
+  describe "Example Tests" $
+    it "should work with example tests" $ do
+      invert [1,2,3,4,5] `shouldBe` [-1,-2,-3,-4,-5]
+      invert [1,-2,3,-4,5] `shouldBe` [-1,2,-3,4,-5]
+      invert [] `shouldBe` []
