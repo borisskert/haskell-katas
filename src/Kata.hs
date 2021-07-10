023eec35821bd6d635c47fba850a9fea6d13f4ec
Boris Skert <boris.skert@gmail.com>
2021-07-10 09:17:56 +0200

    Solve 'I love you, a little , a lot, passionately ... not at all' kata

---
+++ src/Kata.hs
@@ -0,0 +1,13 @@
+module Kata (howMuchILoveYou) where
+
+-- https://www.codewars.com/kata/57f24e6a18e9fad8eb000296/train/haskell
+
+howMuchILoveYou :: Int -> String
+howMuchILoveYou nbPetals
+  | (nbPetals - 1) `mod` 6 == 0 = "I love you"
+  | (nbPetals - 1) `mod` 6 == 1 = "a little"
+  | (nbPetals - 1) `mod` 6 == 2 = "a lot"
+  | (nbPetals - 1) `mod` 6 == 3 = "passionately"
+  | (nbPetals - 1) `mod` 6 == 4 = "madly"
+  | (nbPetals - 1) `mod` 6 == 5 = "not at all"
+  | otherwise = error "Should not happen"
---
+++ test/KataSpec.hs
@@ -0,0 +1,17 @@
+module KataSpec where
+import Kata (howMuchILoveYou)
+import Test.Hspec
+
+spec :: Spec
+spec = do
+  describe "Example Tests" $
+    it "should work with example tests" $ do
+      howMuchILoveYou 1 `shouldBe` "I love you"
+      howMuchILoveYou 2 `shouldBe` "a little"
+      howMuchILoveYou 3 `shouldBe` "a lot"
+      howMuchILoveYou 4 `shouldBe` "passionately"
+      howMuchILoveYou 5 `shouldBe` "madly"
+      howMuchILoveYou 6 `shouldBe` "not at all"
+      howMuchILoveYou 7 `shouldBe` "I love you"
+      howMuchILoveYou 8 `shouldBe` "a little"
+      howMuchILoveYou 9 `shouldBe` "a lot"
