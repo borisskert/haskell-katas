cf4d8be75c64b58ac9444e2c000b7f089bb562ed
Boris Skert <boris.skert@gmail.com>
2021-07-10 20:24:48 +0200

    Solve 'Exclamation marks series #6: Remove n exclamation marks in the sentence from left to right' kata

---
+++ src/Kata.hs
@@ -0,0 +1,10 @@
+module Kata (remove) where
+
+--  https://www.codewars.com/kata/57faf7275c991027af000679/train/haskell
+
+remove :: String -> Int -> String
+remove sentence 0 = sentence
+remove "" _       = ""
+remove (first:others) count
+  | first == '!' = remove others (count - 1)
+  | otherwise    = [first] ++ remove others count
---
+++ test/KataSpec.hs
@@ -0,0 +1,20 @@
+module KataSpec where
+import Kata (remove)
+import Test.Hspec
+
+spec :: Spec
+spec = do
+    describe "Basic Tests" $
+      it "should work with basic tests" $ do 
+        (remove "Hi!" 1)                 `shouldBe` "Hi"
+        (remove "Hi!!!" 100)             `shouldBe` "Hi"
+        (remove "Hi!!!" 1)               `shouldBe` "Hi!!"
+        (remove "Hi!!!" 100)             `shouldBe` "Hi"
+        (remove "Hi! Hi!" 1)             `shouldBe` "Hi Hi!"
+        (remove "!!!Hi !!hi!!! !hi" 1)   `shouldBe` "!!Hi !!hi!!! !hi"
+        (remove "!!!Hi !!hi!!! !hi" 3)   `shouldBe` "Hi !!hi!!! !hi"
+        (remove "!!!Hi !!hi!!! !hi" 5)   `shouldBe` "Hi hi!!! !hi"
+        (remove "!!!Hi !!hi!!! !hi" 100) `shouldBe` "Hi hi hi"
+        (remove "!Hi" 1)                 `shouldBe` "Hi"
+        (remove "!Hi!" 1)                `shouldBe` "Hi!"
+        (remove "!Hi!" 100)              `shouldBe` "Hi"
