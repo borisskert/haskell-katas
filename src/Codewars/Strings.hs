40d45d332cac46681f7f414e877cbd71fdf8ea9c
Boris Skert <boris.skert@gmail.com>
2021-07-11 09:46:04 +0200

    Solve 'Regex count lowercase letters' kata

---
+++ src/Codewars/Strings.hs
@@ -0,0 +1,8 @@
+module Codewars.Strings where
+
+--  https://www.codewars.com/kata/56a946cd7bd95ccab2000055/train/haskell
+
+import Data.Char (isLower)
+
+lowercaseCount :: [Char] -> Int
+lowercaseCount = length . filter isLower
---
+++ test/Codewars/StringsSpec.hs
@@ -0,0 +1,18 @@
+module Codewars.StringsSpec where
+
+import Codewars.Strings (lowercaseCount)
+import Test.Hspec
+import Test.QuickCheck
+
+spec :: Spec
+spec = do
+  describe "lowercaseCount" $ do
+    it "works for some examples" $ do
+      lowercaseCount "abc" `shouldBe` 3
+      lowercaseCount "abcABC" `shouldBe` 3
+      lowercaseCount "abcABC123" `shouldBe` 3
+      lowercaseCount "abcABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~" `shouldBe` 3
+      lowercaseCount "" `shouldBe` 0
+      lowercaseCount "ABC123!@€£#$%^&*()_-+=}{[]|\':;?/>.<,~" `shouldBe` 0
+      lowercaseCount "abcdefghijklmnopqrstuvwxyz" `shouldBe` 26
+
