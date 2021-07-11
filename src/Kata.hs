5303b4f08c0fecba1c2f23ca5dc6a85442b544f5
Boris Skert <boris.skert@gmail.com>
2021-07-11 18:07:37 +0200

    Solve 'Exclamation marks series #4: Remove all exclamation marks from sentence but ensure a exclamation mark at the end of string' kata

---
+++ src/Kata.hs
@@ -0,0 +1,6 @@
+module Kata where
+
+--  https://www.codewars.com/kata/57faf12b21c84b5ba30001b0/train/haskell
+
+remove :: String -> String
+remove s = filter (/= '!') s ++ "!"
---
+++ test/KataSpec.hs
@@ -0,0 +1,14 @@
+module KataSpec where
+import Test.Hspec
+import Kata (remove)
+
+spec :: Spec
+spec = do
+  describe "Example Tests" $
+    it "should work with example tests" $ do
+      remove "Hi!" `shouldBe` "Hi!"
+      remove "Hi!!!" `shouldBe` "Hi!"
+      remove "!Hi" `shouldBe` "Hi!"
+      remove "!Hi!" `shouldBe` "Hi!"
+      remove "Hi! Hi!" `shouldBe` "Hi Hi!"
+      remove "Hi" `shouldBe` "Hi!"
