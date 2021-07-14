57617f4f297b730af9725ef662a0beee7707ecee
Boris Skert <boris.skert@gmail.com>
2021-07-14 18:53:38 +0200

    Solve 'Highest and Lowest' kata

---
+++ src/Kata.hs
@@ -0,0 +1,11 @@
+module Kata (highAndLow) where
+
+--  https://www.codewars.com/kata/554b4ac871d6813a03000035/train/haskell
+
+import Data.List (sort)
+
+
+highAndLow :: String -> String
+highAndLow input = unwords [(last sortedNumbers), (head sortedNumbers)]
+  where
+    sortedNumbers = map show $ sort $ map (read :: String -> Int) $ words input
---
+++ test/KataSpec.hs
@@ -0,0 +1,9 @@
+module KataSpec where
+import Test.Hspec
+import Kata (highAndLow)
+
+spec :: Spec
+spec = do
+  describe "Example Tests" $ do
+    it "4 5 29 54 4 0 -214 542 -64 1 -3 6 -6" $ 
+      highAndLow "4 5 29 54 4 0 -214 542 -64 1 -3 6 -6" `shouldBe`  "542 -214"
