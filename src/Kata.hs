0a7e9a56e3f4858f17cde8a63d4f838495f3fd3f
Boris Skert <boris.skert@gmail.com>
2021-08-03 19:03:38 +0200

    Solve 'Averages of numbers' kata

---
+++ src/Kata.hs
@@ -0,0 +1,12 @@
+module Kata where
+
+-- https://www.codewars.com/kata/57d2807295497e652b000139/train/haskell
+
+averages :: Maybe [Double] -> [Double]
+averages Nothing = []
+averages (Just xs) = avg xs
+  where
+    avg :: [Double] -> [Double]
+    avg [] = []
+    avg [_] = []
+    avg (x1:x2:others) = ((x1 + x2) / 2) : avg (x2:others)
