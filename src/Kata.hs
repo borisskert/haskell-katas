f8a26d5d440acfc32e3e27199618d532a8748195
Boris Skert <boris.skert@gmail.com>
2021-08-03 18:42:29 +0200

    Solve 'Thinkful - String Drills: Repeater' kata

---
+++ src/Kata.hs
@@ -0,0 +1,8 @@
+module Kata where
+
+-- https://www.codewars.com/kata/585a1a227cb58d8d740001c3/train/haskell
+
+repeater :: String -> Int -> String
+repeater string n = take count $ cycle string
+  where
+    count = length string * n
---
+++ test/KataSpec.hs
@@ -0,0 +1,12 @@
+module KataSpec where
+
+import Kata (repeater)
+import Test.Hspec
+
+spec :: Spec
+spec = do
+  describe "Example Tests" $
+    it "should work with example tests" $ do
+      repeater "a"    5  `shouldBe` "aaaaa"
+      repeater "Wub " 6  `shouldBe` "Wub Wub Wub Wub Wub Wub "
+      repeater "Na"   16 `shouldBe` "NaNaNaNaNaNaNaNaNaNaNaNaNaNaNaNa"
