b9642120ad00e4d89a41c8d69ef37f8960af316b
Boris Skert <boris.skert@gmail.com>
2021-08-05 18:02:46 +0200

    Solve 'Halving Sum' kata

---
+++ src/Kata.hs
@@ -0,0 +1,17 @@
+module Kata (halvingSum) where
+
+-- https://www.codewars.com/kata/5a58d46cfd56cb4e8600009d/train/haskell
+
+-- best practice:
+-- halvingSum :: Int -> Int
+-- halvingSum 1 = 1
+-- halvingSum  n = n + halvingSum (div n 2)
+
+halvingSum :: Int -> Int
+halvingSum 1 = 1
+halvingSum n = sum (map (\x -> n `div` x) (dividers 1)) - 1
+  where
+    dividers :: Int -> [Int]
+    dividers x
+      | x >= n = [n]
+      | otherwise = x : dividers (x * 2)
---
+++ test/KataSpec.hs
@@ -0,0 +1,18 @@
+module KataSpec (spec) where
+
+import Test.Hspec
+import Kata (halvingSum)
+
+spec :: Spec
+spec = do
+  it "Basic tests" $ do
+    halvingSum 25 `shouldBe` 47
+    halvingSum 127 `shouldBe` 247
+    halvingSum 38 `shouldBe` 73
+    halvingSum 1 `shouldBe` 1
+    halvingSum 320 `shouldBe` 638
+    halvingSum 13 `shouldBe` 23
+    halvingSum 15 `shouldBe` 26
+    halvingSum 47 `shouldBe` 89
+    halvingSum 101 `shouldBe` 198
+    halvingSum 257 `shouldBe` 512
