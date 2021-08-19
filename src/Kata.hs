161ccf41125544a39c8414cb0430f5b0eacdef8c
Boris Skert <boris.skert@gmail.com>
2021-08-19 18:54:15 +0200

    Solve 'Compare Strings by Sum of Chars' kata

---
+++ src/Kata.hs
@@ -0,0 +1,22 @@
+module Kata where
+
+-- https://www.codewars.com/kata/576bb3c4b1abc497ec000065/train/haskell
+
+import Data.Char (isLetter, toUpper, ord)
+
+
+compare' :: Maybe String -> Maybe String -> Bool
+compare' Nothing Nothing = True
+compare' (Just s1) Nothing = toCharSum s1 == 0
+compare' Nothing (Just s2) = toCharSum s2 == 0
+compare' (Just s1) (Just s2) = toCharSum s1 == toCharSum s2
+
+
+toCharSum :: String -> Int 
+toCharSum text
+  | isApplicable text = sum $ map (ord . toUpper) text
+  | otherwise = 0
+
+
+isApplicable :: String -> Bool
+isApplicable = all isLetter
---
+++ test/KataSpec.hs
@@ -0,0 +1,20 @@
+module KataSpec (spec) where
+import Test.Hspec
+import Kata
+
+main = hspec spec
+spec = do
+  describe "Example Tests" $
+    it "should work with example tests" $ do
+      compare' (Just "AD")   (Just "BC")    `shouldBe` True
+      compare' (Just "AD")   (Just "DD")    `shouldBe` False
+      compare' (Just "gf")   (Just "FG")    `shouldBe` True
+      compare' (Just "Ad")   (Just "DD")    `shouldBe` False
+      compare' (Just "zz1")  (Just "")      `shouldBe` True
+      compare' (Just "ZzZz") (Just "ffPFF") `shouldBe` True
+      compare' (Just "kl")   (Just "lz")    `shouldBe` False
+      compare' Nothing       (Just "")      `shouldBe` True
+      compare' (Just "!!")   (Just "7476")  `shouldBe` True
+      compare' (Just "##")   (Just "1176")  `shouldBe` True
+      compare' (Just "a1")   Nothing        `shouldBe` True
+      compare' Nothing       (Just "a1")    `shouldBe` True
