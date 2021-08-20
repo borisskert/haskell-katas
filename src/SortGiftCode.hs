0efa069b97081786c75dc4254f09f23e2371641a
Boris Skert <boris.skert@gmail.com>
2021-08-20 18:53:57 +0200

    Sort 'Sort the Gift Code' kata

---
+++ src/SortGiftCode.hs
@@ -0,0 +1,8 @@
+module SortGiftCode where
+
+-- https://www.codewars.com/kata/52aeb2f3ad0e952f560005d3/train/haskell
+
+import Data.List (sort)
+
+sortGiftCode :: String -> String
+sortGiftCode = sort
