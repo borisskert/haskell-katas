7d868ef743b6dd207830b0a3a6d8ac193e963895
Boris Skert <boris.skert@gmail.com>
2021-08-20 18:47:36 +0200

    Solve 'Return the first M multiples of N' kata

---
+++ src/Multiples.hs
@@ -0,0 +1,6 @@
+module Multiples where
+
+-- https://www.codewars.com/kata/593c9175933500f33400003e/train/haskell
+
+multiples :: Int -> Double -> [Double]
+multiples n value = map ((* value) . fromIntegral) [1 .. n]
