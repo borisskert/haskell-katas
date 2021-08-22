3fc5dd3a3e22228f96a8619c2c3fc1b4abb321e6
Boris Skert <boris.skert@gmail.com>
2021-08-22 18:25:16 +0200

    Solve 'Complete The Pattern #2' kata

---
+++ src/Haskell/Codewars/Pattern.hs
@@ -0,0 +1,13 @@
+module Haskell.Codewars.Pattern where
+
+-- https://www.codewars.com/kata/55733d3ef7c43f8b0700007c/train/haskell
+
+pattern :: Int -> String
+pattern 1 = "1"
+pattern n
+  | n > 1 = patternOf [n, (n-1) .. 1]
+  | otherwise = ""
+  where
+    patternOf :: [Int] -> String 
+    patternOf [c] = show c
+    patternOf text = concatMap show text ++ "\n" ++ patternOf (init text)
