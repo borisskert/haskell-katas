6aa457df26a8a7e54c03b0d89b15bc4cd549f8fd
Boris Skert <boris.skert@gmail.com>
2021-08-27 19:28:43 +0200

    Solve 'Mirror, mirror, on the wall...' kata

---
+++ src/Mirror.hs
@@ -0,0 +1,13 @@
+module Mirror (mirror) where
+
+-- https://www.codewars.com/kata/5f55ecd770692e001484af7d/train/haskell
+
+import Data.List (sort)
+
+
+mirror :: [Int] -> [Int]
+mirror [] = []
+mirror xs = sortedInit ++ last sorted : reverse sortedInit
+  where
+    sorted = sort xs
+    sortedInit = init sorted
