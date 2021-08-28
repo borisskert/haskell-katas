26ec6fbc758bd2b285e539415c6092ef76a1941d
Boris Skert <boris.skert@gmail.com>
2021-08-28 08:59:03 +0200

    Solve 'Happy Birthday, Darling\!' kata

---
+++ src/Kata.hs
@@ -0,0 +1,26 @@
+module Kata (womensAge) where
+
+-- https://www.codewars.com/kata/5e96332d18ac870032eb735f/train/haskell
+
+--better use printf:
+-- import Text.Printf (printf)
+
+-- womensAge :: Int -> String
+-- womensAge n = printf "%d? That's just 2%d, in base %d!" n (mod n 2) (div n 2)
+
+
+womensAge :: Int -> String
+womensAge x = show x ++ "? That's just " ++ show realAge ++ ", in base " ++ show base ++ "!"
+  where
+    base = toBase x
+    realAge = toRealAge x
+
+
+toRealAge :: Int -> Int
+toRealAge x
+  | even x = 20
+  | otherwise = 21
+
+
+toBase :: Int -> Int 
+toBase = (`div` 2)
