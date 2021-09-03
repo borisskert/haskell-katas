954bc18fe3c66d0db5429ad3379458b432ab517b
Boris Skert <boris.skert@gmail.com>
2021-09-03 18:35:58 +0200

    Solve 'Fizz Buzz Cuckoo Clock' kata

---
+++ src/Kata.hs
@@ -0,0 +1,45 @@
+module Kata where
+
+-- https://www.codewars.com/kata/58485a43d750d23bad0000e6/train/haskell
+
+import Data.List.Split.Internals (splitOn)
+
+
+fizzBuzzCuckooClock :: String -> String
+fizzBuzzCuckooClock time
+  | minutes == 0 = cuckoo hours
+  | minutes == 30 = "Cuckoo"
+  | minutes `mod` 5 == 0 && minutes `mod` 3 == 0 = "Fizz Buzz"
+  | minutes `mod` 5 == 0 = "Buzz"
+  | minutes `mod` 3 == 0 = "Fizz"
+  | otherwise = "tick"
+  where
+    parsed = parse time :: Time
+    hours = toHours parsed
+    minutes = toMinutes parsed
+
+
+cuckoo :: Int -> String 
+cuckoo hours
+  | count == 0 = unwords . replicate 12 $ "Cuckoo"
+  | otherwise = unwords . replicate count $ "Cuckoo"
+  where
+    count = hours `mod` 12
+
+
+toHours :: Time -> Int 
+toHours (Time (hours, _)) = hours
+
+
+toMinutes :: Time -> Int 
+toMinutes (Time (_, minutes)) = minutes
+
+
+-- Time (hours, minutes)
+newtype Time = Time (Int, Int)
+
+
+parse :: String -> Time
+parse time = Time (read (head split'), read (last split'))
+  where
+    split' = splitOn ":" time :: [String]
---
+++ test/KataSpec.hs
@@ -0,0 +1,17 @@
+module KataSpec where
+import Test.Hspec
+import Kata
+
+
+spec :: Spec
+spec = do
+  describe "Example Tests" $ 
+    it "should work with example tests" $ do
+      fizzBuzzCuckooClock "13:34" `shouldBe` "tick"
+      fizzBuzzCuckooClock "21:00" `shouldBe` "Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo"
+      fizzBuzzCuckooClock "11:15" `shouldBe` "Fizz Buzz"
+      fizzBuzzCuckooClock "03:03" `shouldBe` "Fizz"
+      fizzBuzzCuckooClock "14:30" `shouldBe` "Cuckoo"
+      fizzBuzzCuckooClock "08:55" `shouldBe` "Buzz"
+      fizzBuzzCuckooClock "00:00" `shouldBe` "Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo"
+      fizzBuzzCuckooClock "12:00" `shouldBe` "Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo Cuckoo"
