7762ad530268d43e0820334ce8253326215263d7
Boris Skert <boris.skert@gmail.com>
2021-09-04 11:09:23 +0200

    Solve 'Consecutive Ducks' kata

---
+++ src/Kata.hs
@@ -0,0 +1,9 @@
+module Kata (consecutiveDucks) where
+
+-- https://www.codewars.com/kata/5dae2599a8f7d90025d2f15f/train/haskell
+
+consecutiveDucks :: Int -> Bool
+consecutiveDucks 1 = False
+consecutiveDucks n
+  | odd n = True 
+  | otherwise = consecutiveDucks (n `div` 2)
