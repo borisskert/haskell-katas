57599f6528e402005978a6420e54ab791e82522c
Boris Skert <boris.skert@gmail.com>
2021-09-21 09:22:01 +0200

    Solve 'Product of Array Items' kata

---
+++ src/Product.hs
@@ -0,0 +1,7 @@
+module Product where
+
+-- https://www.codewars.com/kata/5901f361927288d961000013/train/haskell
+
+product' :: [Integer] -> Maybe Integer
+product' [] = Nothing
+product' xs = Just (product xs)
---
+++ test/ProductSpec.hs
@@ -0,0 +1,17 @@
+module ProductSpec where
+
+import Test.Hspec
+import Test.QuickCheck
+import Product
+
+spec :: Spec
+spec = do
+  it "finds the product" $ do
+   product' [5, 4, 1, 3, 9] `shouldBe` Just 540
+   product' [-2, 6, 7, 8] `shouldBe` Just (-672)
+   product' [10] `shouldBe` Just 10
+   product' [0, 2, 9, 7] `shouldBe` Just 0
+   product' [] `shouldBe` Nothing
+ 
+-- the following line is optional for 8.2
+main = hspec spec
