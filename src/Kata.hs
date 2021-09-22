d67bdc796322d80524c6f7c7a69b751cfa2252c3
Boris Skert <boris.skert@gmail.com>
2021-09-22 18:40:37 +0200

    Solve 'Exclamation marks series #5: Remove all exclamation marks from the end of words' kata

---
+++ src/Kata.hs
@@ -0,0 +1,12 @@
+module Kata (remove) where
+
+-- https://www.codewars.com/kata/57faf32df815ebd49e000117/train/haskell
+
+remove :: String -> String
+remove = unwords . map removeFromEnd . words
+
+
+removeFromEnd :: String -> String
+removeFromEnd word
+  | last word == '!' = removeFromEnd (init word)
+  | otherwise = word
