9e6727fb24990a8325eaa6a9d4678f7cdf7ee105
Boris Skert <boris.skert@gmail.com>
2021-09-29 10:29:17 +0200

    Solve 'Calculate mean and concatenate string' kata

---
+++ src/Kata.hs
@@ -0,0 +1,14 @@
+module Kata (mean) where
+
+-- https://www.codewars.com/kata/56f7493f5d7c12d1690000b6/train/haskell
+
+import Data.Char (isDigit, digitToInt, isLetter)
+
+
+mean :: [Char] -> (Double, String)
+mean lst = (avg, letters)
+  where
+    numbers = map (fromIntegral . digitToInt) . filter isDigit $ lst :: [Double]
+    count = fromIntegral . length $ numbers :: Double
+    avg = sum numbers / count :: Double
+    letters = filter isLetter lst :: [Char]
