44a660329ddbd6eeb9bbc33da45a65a132bd62a1
Boris Skert <boris.skert@gmail.com>
2021-09-29 08:47:47 +0200

    Solve 'Exclamation marks series #3: Remove all exclamation marks from sentence except at the end' kata

---
+++ src/Kata.hs
@@ -0,0 +1,9 @@
+module Kata (remove) where
+
+-- https://www.codewars.com/kata/57faefc42b531482d5000123/train/haskell
+
+remove :: String -> String
+remove [] = []
+remove s
+  | last s == '!' = remove (init s) ++ "!"
+  | otherwise = filter (/= '!') s
