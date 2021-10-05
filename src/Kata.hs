cf242cab270727ad2d9f17c02e8ef7925705fb40
Boris Skert <boris.skert@gmail.com>
2021-10-05 19:54:58 +0200

    Solve 'Tail Swap' kata

---
+++ src/Kata.hs
@@ -0,0 +1,17 @@
+module Kata (tailSwap) where
+
+-- https://www.codewars.com/kata/5868812b15f0057e05000001/train/haskell
+
+import Data.List.Split (splitOn)
+
+
+tailSwap :: (String, String) -> (String, String)
+tailSwap strings = (first, second)
+  where
+    separator = ":" :: String
+
+    toSplit :: String -> [String]
+    toSplit = splitOn separator
+
+    first = (head . toSplit . fst $ strings) ++ separator ++ (last . toSplit . snd $ strings) :: String
+    second = (head . toSplit . snd $ strings) ++ separator ++ (last . toSplit . fst $ strings) :: String
