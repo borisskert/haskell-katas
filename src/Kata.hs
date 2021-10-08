b6bd34759016057e95a86f73ae1388e924abb7c4
Boris Skert <boris.skert@gmail.com>
2021-10-08 18:24:14 +0200

    Solve 'Help Suzuki rake his garden\!' kata

---
+++ src/Kata.hs
@@ -0,0 +1,10 @@
+module Kata (rakeGarden) where
+
+-- https://www.codewars.com/kata/571c1e847beb0a8f8900153d/train/haskell
+
+rakeGarden :: String -> String
+rakeGarden = unwords . map rake . words
+  where
+    rake :: String -> String
+    rake "rock" = "rock"
+    rake _ = "gravel"
