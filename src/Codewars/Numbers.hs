cd5fa75780fcdd07107c2ed49833956c57bc34a0
Boris Skert <boris.skert@gmail.com>
2021-10-08 18:40:30 +0200

    Solve 'Beginner Series #5 Triangular Numbers' kata

---
+++ src/Codewars/Numbers.hs
@@ -0,0 +1,14 @@
+module Codewars.Numbers where
+
+-- https://www.codewars.com/kata/56d0a591c6c8b466ca00118b/train/haskell
+
+isTriangular :: Int -> Bool
+isTriangular t = t == getY x
+  where
+    x = getX t :: Int
+
+    getY :: Int -> Int 
+    getY x = x * (x + 1) `div` 2
+
+    getX :: Int -> Int
+    getX c = floor (- 0.5 + sqrt (0.25 + 2 * fromIntegral c))
