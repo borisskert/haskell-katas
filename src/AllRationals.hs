80ab774fea17804d3b2abd0f1ed4e4547f1dd7eb
Boris Skert <boris.skert@gmail.com>
2021-10-13 19:03:20 +0200

    Solve 'List of all Rationals' kata

---
+++ src/AllRationals.hs
@@ -0,0 +1,10 @@
+module AllRationals where
+
+-- https://www.codewars.com/kata/5e4e8f5a72d9550032953717/train/haskell
+
+allRationals :: [(Integer, Integer)]
+allRationals = concat . iterate next $ [(1, 1)]
+  where
+    next :: [(Integer, Integer)] -> [(Integer, Integer)]
+    next [] = []
+    next ((a, b):others) = [(a, b + a), (a + b, b)] ++ next others
