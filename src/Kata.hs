6ce1a73607a4536452dc450804d96f3e7a9ee8e8
Boris Skert <boris.skert@gmail.com>
2021-10-20 17:21:36 +0200

    Solve 'Series of integers from 0 to n' kata

---
+++ src/Kata.hs
@@ -0,0 +1,6 @@
+module Kata (generateIntegers) where
+
+-- https://www.codewars.com/kata/5841f4fb673ea2a2ae000111/train/haskell
+
+generateIntegers :: Int -> [Int]
+generateIntegers n = [0 .. n]
---
+++ test/KataSpec.hs
@@ -0,0 +1,11 @@
+module KataSpec where
+import Kata (generateIntegers)
+import Test.Hspec
+
+spec :: Spec
+spec = do
+  describe "Example Tests" $
+    it "should work with example tests" $ do
+      generateIntegers 3 `shouldBe` [0,1,2,3]
+      generateIntegers 4 `shouldBe` [0,1,2,3,4]
+      generateIntegers 5 `shouldBe` [0,1,2,3,4,5]
