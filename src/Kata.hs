1185ed2901813c5c7a5e54dcc567a384163b80de
Boris Skert <boris.skert@gmail.com>
2021-10-25 19:53:04 +0200

    Solve 'How many times does it contain?' kata

---
+++ src/Kata.hs
@@ -0,0 +1,6 @@
+module Kata (stringCounter) where
+
+-- https://www.codewars.com/kata/584466950d3bedb9b300001f/train/haskell
+
+stringCounter :: String -> Char -> Int
+stringCounter inputS charS = length . filter (== charS) $ inputS
