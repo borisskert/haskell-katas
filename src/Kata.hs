b17cb8bcdbb166a622afe1d60842ea8f994e6f10
Boris Skert <boris.skert@gmail.com>
2021-10-28 19:39:43 +0200

    Solve 'Exclamation marks series #8: Move all exclamation marks to the end of the sentence' kata

---
+++ src/Kata.hs
@@ -0,0 +1,16 @@
+module Kata (remove) where
+
+-- https://www.codewars.com/kata/57fafd0ed80daac48800019f/train/haskell
+
+remove :: String -> String
+remove s = text ++ exclamationMarks
+  where
+    exclamationMarks = filter (== '!') s :: String
+    text = filter (/= '!') s :: String
+
+
+-- best practice:
+-- import Data.List
+
+-- remove :: String -> String
+-- remove = sortOn (=='!')
---
+++ test/KataSpec.hs
@@ -0,0 +1,14 @@
+module KataSpec where
+import Kata (remove)
+import Test.Hspec
+
+
+spec :: Spec
+spec = do
+  describe "Example Tests" $
+    it "should work with example tests" $ do
+      remove "Hi!"          `shouldBe` "Hi!"
+      remove "Hi! Hi!"      `shouldBe` "Hi Hi!!"
+      remove "Hi! Hi! Hi!"  `shouldBe` "Hi Hi Hi!!!"
+      remove "Hi! !Hi Hi!"  `shouldBe` "Hi Hi Hi!!!"
+      remove "Hi! Hi!! Hi!" `shouldBe` "Hi Hi Hi!!!!"
