0aca88cf1e3e850959c9cd09ae3852312f386da2
Boris Skert <boris.skert@gmail.com>
2021-11-04 18:43:10 +0100

    Solve 'Method For Counting Total Occurence Of Specific Digits' kata

---
+++ src/Count.hs
@@ -0,0 +1,17 @@
+module Count where
+
+-- https://www.codewars.com/kata/56311e4fdd811616810000ce/train/haskell
+
+import Data.Char (digitToInt)
+
+countSpecDigits :: [Int] -> [Int] -> [(Int,Int)]
+countSpecDigits xs = map (\d -> (d, toCount d))
+  where
+    digits = concatMap toDigits xs :: [Int]
+
+    toCount :: Int -> Int
+    toCount d = length . filter (== d) $ digits
+
+
+toDigits :: Int -> [Int]
+toDigits = map digitToInt . show . abs
