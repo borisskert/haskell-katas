ff187966f10dc188ce6f00028c78a34bbf17b4c5
Boris Skert <boris.skert@gmail.com>
2021-11-09 17:59:45 +0100

    Solve 'Holiday II - Plane Seating' kata

---
+++ src/Kata.hs
@@ -0,0 +1,31 @@
+module Kata (planeSeat) where
+
+-- https://www.codewars.com/kata/57e8f757085f7c7d6300009a/train/haskell
+
+import Data.Char (isLetter, isNumber)
+import Data.List (intercalate)
+
+planeSeat :: String -> String
+planeSeat seat
+  | null cluster || null section = "No Seat!!"
+  | otherwise = intercalate "-" [section, cluster]
+  where
+    number = read . filter isNumber $ seat :: Int
+    letter = head . filter isLetter $ seat :: Char
+
+    section = toSection number :: String
+    cluster = toCluster letter :: String
+
+    toCluster :: Char -> String
+    toCluster c
+      | c `elem` "ABC" = "Left"
+      | c `elem` "DEF" = "Middle"
+      | c `elem` "GHK" = "Right"
+      | otherwise = []
+
+    toSection :: Int -> String
+    toSection n
+      | n <= 20 = "Front"
+      | n <= 40 = "Middle"
+      | n <= 60 = "Back"
+      | otherwise = []
---
+++ test/KataSpec.hs
@@ -0,0 +1,15 @@
+module KataSpec (spec) where
+
+import Test.Hspec
+import Kata (planeSeat)
+
+spec :: Spec
+spec = do
+  it "Basic tests" $ do
+    planeSeat "2B" `shouldBe` "Front-Left"
+    planeSeat "20B" `shouldBe` "Front-Left"
+    planeSeat "58I" `shouldBe` "No Seat!!"
+    planeSeat "60D" `shouldBe` "Back-Middle"
+    planeSeat "17K" `shouldBe` "Front-Right"
+    planeSeat "26J" `shouldBe` "No Seat!!"
+    planeSeat "3H" `shouldBe` "Front-Right"
