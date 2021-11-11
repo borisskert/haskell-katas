733a43442f04d73369c75fcbb169150f55346e01
Boris Skert <boris.skert@gmail.com>
2021-11-11 18:17:36 +0100

    Solve 'Thinkful - String Drills: Areacode extractor' kata

---
+++ src/Kata.hs
@@ -0,0 +1,6 @@
+module Kata (areaCode) where
+
+-- https://www.codewars.com/kata/585a36b445376cbc22000072/train/haskell
+
+areaCode :: String -> String
+areaCode = takeWhile (/= ')') . drop 1 . dropWhile (/= '(')
---
+++ test/KataSpec.hs
@@ -0,0 +1,12 @@
+module KataSpec where
+
+import Kata (areaCode)
+import Test.Hspec
+
+spec :: Spec
+spec = do
+  describe "Example Test" $
+    it "should work with example tests" $ do
+      areaCode "The supplier's phone number is (555) 867-5309" `shouldBe` "555"
+      areaCode "Grae's cell number used to be (123) 456-7890" `shouldBe` "123"
+      areaCode "The 102nd district court's fax line is (124) 816-3264" `shouldBe` "124"
