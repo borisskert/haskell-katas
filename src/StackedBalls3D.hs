16653fc3bcfae5dd638dd0aab129da4f834f2ce0
Boris Skert <boris.skert@gmail.com>
2021-11-12 20:55:26 +0100

    Solve 'Stacked Balls - 3D (square base)' kata

---
+++ src/StackedBalls3D.hs
@@ -0,0 +1,14 @@
+module StackedBalls3D (stack) where
+
+-- https://www.codewars.com/kata/5bb493932ce53339dc0000c2/train/haskell
+
+stack :: Int -> Double
+stack 0 = 0.0
+stack n = round4 . (+ 1.0) . (* sin45) . fromIntegral . (+ (-1)) $ n
+  where
+    sin45 = sqrt 2.0 / 2.0
+
+round4 :: Double -> Double
+round4 = (/ 10 ^ n) . fromInteger . round . (* 10.0 ^ n)
+  where
+    n = 4
