f9f1d504792548aaa018d91aebe8e0899ce8980e
Boris Skert <boris.skert@gmail.com>
2021-11-16 20:51:25 +0100

    Solve 'Multiples By Permutations II' kata

---
+++ src/Multiples.hs
@@ -0,0 +1,18 @@
+module Multiples where
+
+-- https://www.codewars.com/kata/5ba178be875de960a6000187/train/haskell
+
+import Data.List (sort)
+
+findLowestInt :: Int -> Int
+findLowestInt k1 =
+  fst
+    . head
+    . filter (\(_, (x, y)) -> toDigits x == toDigits y)
+    . map (\x -> (x, (x * k1, x * k2)))
+    $ [1 ..]
+  where
+    k2 = k1 + 1
+
+toDigits :: Int -> String
+toDigits = sort . show
