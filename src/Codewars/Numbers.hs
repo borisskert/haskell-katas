d80e2e3754066cb61102f1156c09a1cb7c1b2f99
Boris Skert <boris.skert@gmail.com>
2021-11-16 17:47:40 +0100

    Solve 'Excessively Abundant Numbers' kata

---
+++ src/Codewars/Numbers.hs
@@ -0,0 +1,9 @@
+module Codewars.Numbers where
+
+-- https://www.codewars.com/kata/56a75b91688b49ad94000015/train/haskell
+
+abundantNumber :: Int -> Bool
+abundantNumber x = (> x) . sum . divisors $ x
+
+divisors :: Int -> [Int]
+divisors i = filter (\x -> i `mod` x == 0) [2 .. (i `div` 2)]
