e4221c1d8410ebc5f21d2daf306e6ba97409f17b
Boris Skert <boris.skert@gmail.com>
2021-11-17 21:27:13 +0100

    Solve 'Find the missing letter' kata

---
+++ src/Kata.hs
@@ -0,0 +1,13 @@
+module Kata where
+
+-- https://www.codewars.com/kata/5839edaa6754d6fec10000a2/train/haskell
+
+import Data.Char (isLower)
+
+findMissingLetter :: [Char] -> Char
+findMissingLetter letters =
+  fst
+    . head
+    . filter (uncurry (/=))
+    . zip [(head letters) .. (last letters)]
+    $ letters
---
+++ test/KataSpec.hs
@@ -0,0 +1,10 @@
+module KataSpec where
+import Test.Hspec
+import Kata
+
+spec :: Spec
+spec = do
+  describe "Example Tests" $
+    it "should work with example tests" $ do
+      findMissingLetter ['a','b','c','d','f'] `shouldBe` 'e'
+      findMissingLetter ['O','Q','R','S'] `shouldBe` 'P'
