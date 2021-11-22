909b9a1a033a2f715082ef51d660dadff6321672
Boris Skert <boris.skert@gmail.com>
2021-11-22 18:42:10 +0100

    Solve 'Count number of zeros from 1 to N' kata

---
+++ src/Count.hs
@@ -0,0 +1,6 @@
+module Count where
+
+-- https://www.codewars.com/kata/557cffec8c3e8e55cc00010f/train/haskell
+
+countZeros :: Int -> Int
+countZeros a = length . filter (== '0') . concatMap show $ [1 .. a]
