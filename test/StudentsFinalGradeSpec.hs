3903699bf628bba72317f078f596eeae1aa633bd
Boris Skert <boris.skert@gmail.com>
2022-03-30 17:41:05 +0100

    Solve 'Student's Final Grade' kata

---
+++ test/StudentsFinalGradeSpec.hs
@@ -0,0 +1,16 @@
+module StudentsFinalGradeSpec (spec) where
+
+import StudentsFinalGrade (finalGrade)
+import Test.Hspec
+
+spec :: Spec
+spec = do
+  it "example tests" $ do
+    finalGrade 100 12 `shouldBe` 100
+    finalGrade  99  0 `shouldBe` 100
+    finalGrade  10 15 `shouldBe` 100
+    finalGrade  85  5 `shouldBe`  90
+    finalGrade  55  3 `shouldBe`  75
+    finalGrade  55  0 `shouldBe`   0
+    finalGrade  20  2 `shouldBe`   0
+    finalGrade  51  2 `shouldBe`   75
