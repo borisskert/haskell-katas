f596615c2b62ee0d349256c6c9e2a05862a1c471
Boris Skert <boris.skert@gmail.com>
2022-04-02 08:50:46 +0100

    Solve 'Function 2 - squaring an argument' kata

---
+++ src/Kata.hs
@@ -0,0 +1,9 @@
+module Kata where
+
+-- https://www.codewars.com/kata/523b623152af8a30c6000027/train/haskell
+
+square :: Integer -> Integer
+square = floor . (** 2) . fromIntegral
+
+-- use:
+-- square = (^2)
---
+++ test/ExampleSpec.hs
@@ -0,0 +1,15 @@
+module ExampleSpec where
+
+import Test.Hspec
+import Kata (square)
+
+spec :: Spec
+spec = do
+    describe "Testing" $ do
+        it "Sample tests" $ do
+            (square 2) `shouldBe` (4 :: Integer)
+            (square 3) `shouldBe` (9 :: Integer)
+            (square 0) `shouldBe` (0 :: Integer)
+            (square 1) `shouldBe` (1 :: Integer)
+
+main = hspec spec
