37adf5e5384dce727ff2b3a411beb4fe649c1dd6
Boris Skert <boris.skert@gmail.com>
2022-05-10 17:38:43 +0200

    Solve 'Sums of Parts' kata

---
+++ src/Solution.hs
@@ -0,0 +1,12 @@
+module Solution where
+
+--  https://www.codewars.com/kata/5ce399e0047a45001c853c2b/train/haskell
+
+partsSum :: [Integer] -> [Integer]
+partsSum xs = sum' : go sum' xs
+  where
+    sum' = sum xs
+    
+    go :: Integer -> [Integer] -> [Integer]
+    go _ [] = []
+    go s (x:xs) = s - x : go (s - x) xs
