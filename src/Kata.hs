186cc0abe69bcb11bb6f062fc7db73b3fbd48b23
Boris Skert <boris.skert@gmail.com>
2022-06-02 17:30:05 +0200

    Solve 'Decipher this!' kata

---
+++ src/Kata.hs
@@ -0,0 +1,18 @@
+module Kata (decipherThis) where
+
+--  https://www.codewars.com/kata/581e014b55f2c52bb00000f8/train/haskell
+
+import Data.Char (chr, isDigit)
+
+decipherThis :: String -> String
+decipherThis = unwords . map decipherWord . words
+
+decipherWord :: String -> String
+decipherWord word
+  | null rest = [chr code]
+  | null . init $ rest = chr code : rest
+  | otherwise = chr code : last rest : (tail . init $ rest) ++ [second]
+  where
+    code = read . filter isDigit $ word :: Int
+    rest = filter (not . isDigit) word :: String
+    second = head rest :: Char
