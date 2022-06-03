bbf843b4825cd483b023bfecf81e9a0ffebff84b
Boris Skert <boris.skert@gmail.com>
2022-06-03 18:11:57 +0200

    Solve 'Character with longest consecutive repetition' kata

---
+++ src/Kata.hs
@@ -0,0 +1,21 @@
+module Kata (longestRepetition) where
+
+-- https://www.codewars.com/kata/586d6cefbcc21eed7a001155/train/haskell
+
+longestRepetition :: String -> Maybe (Char, Int)
+longestRepetition input
+  | null found = Nothing
+  | otherwise = Just (head found, length found)
+  where
+    found = snd . foldl counting ([], []) $ input
+
+    counting :: ([Char], [Char]) -> Char -> ([Char], [Char])
+    counting (collected, collectedMax) c = (collected', collectedMax')
+      where
+        collected'
+          | null collected = [c]
+          | head collected == c = c : collected
+          | otherwise = [c]
+        collectedMax'
+          | length collected' > length collectedMax = collected'
+          | otherwise = collectedMax
---
+++ test/KataSpec.hs
@@ -0,0 +1,16 @@
+module KataSpec where
+import Kata (longestRepetition)
+import Test.Hspec
+
+spec :: Spec
+spec = describe "Example Tests" $
+    it "should work work with example tests" $ do
+        longestRepetition "aaaabb"      `shouldBe` Just ('a', 4)
+        longestRepetition "bbbaaabaaaa" `shouldBe` Just ('a', 4)
+        longestRepetition "cbdeuuu900"  `shouldBe` Just ('u', 3)
+        longestRepetition "abbbbb"      `shouldBe` Just ('b', 5)
+        longestRepetition "aabb"        `shouldBe` Just ('a', 2)
+        longestRepetition ""            `shouldBe` Nothing
+        
+main :: IO ()
+main = hspec spec
