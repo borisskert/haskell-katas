6c5b06ded8df4d38facb4b663e6b48f05cdddced
Boris Skert <boris.skert@gmail.com>
2022-08-30 18:06:26 +0200

    Solve 'Sorting on planet Twisted-3-7' kata

---
+++ src/Kata.hs
@@ -0,0 +1,29 @@
+module Kata where
+
+--  https://www.codewars.com/kata/58068479c27998b11900056e/train/haskell
+
+import Data.Char (digitToInt)
+import Data.List (sortOn)
+
+sortTwisted37 :: [Int] -> [Int]
+sortTwisted37 = sortOn twisted37ToInt
+
+twisted37ToInt :: Int -> Int
+twisted37ToInt n = digitsToInt (sign, map twisted37Digit digits)
+  where
+    (sign, digits) = intToDigits n
+
+twisted37Digit :: Int -> Int
+twisted37Digit 3 = 7
+twisted37Digit 7 = 3
+twisted37Digit n = n
+
+intToDigits :: Int -> (Int, [Int])
+intToDigits n
+  | n < 0 = (-1, map digitToInt . tail $ digits)
+  | otherwise = (1, map digitToInt digits)
+  where
+    digits = show n
+
+digitsToInt :: (Int, [Int]) -> Int
+digitsToInt (sign, digits) = (sign *) . read . concatMap show $ digits
---
+++ test/KataSpec.hs
@@ -0,0 +1,13 @@
+module KataSpec where
+
+import Kata
+import Test.Hspec
+
+spec :: Spec
+spec = do
+  describe "Example Tests" $
+    it "should work with example tests" $ do
+      sortTwisted37 [1, 2, 3, 4, 5, 6, 7, 8, 9] `shouldBe` [1, 2, 7, 4, 5, 6, 3, 8, 9]
+      sortTwisted37 [12, 13, 14] `shouldBe` [12, 14, 13]
+      sortTwisted37 [9, 2, 4, 7, 3] `shouldBe` [2, 7, 4, 3, 9]
+      sortTwisted37 [9, -9, -11, 3, -10] `shouldBe` [-11, -10, -9, 3, 9]
