fb2d936d9b06de799990f8c05b44975a0b27fa60
Boris Skert <boris.skert@gmail.com>
2022-09-11 13:23:01 +0200

    Solve 'Moves in squared strings (III)' kata

---
+++ src/Codewars/G964/Opstrings1.hs
@@ -0,0 +1,34 @@
+module Codewars.G964.Opstrings1 (rot90Clock, diag1Sym, selfieAndDiag1, oper) where
+
+-- https://www.codewars.com/kata/56dbeec613c2f63be4000be6/train/haskell
+
+import Data.List (intercalate)
+
+rot90Clock :: [Char] -> [Char]
+rot90Clock strng = intercalate "\n" . map (\x -> map (!! x) . reverse $ ls) $ [0 .. (n - 1)]
+  where
+    ls = lines strng :: [String]
+    n = length ls :: Int
+
+diag1Sym :: [Char] -> [Char]
+diag1Sym = intercalate "\n" . diag1SymLines
+
+selfieAndDiag1 :: [Char] -> [Char]
+selfieAndDiag1 strng = intercalate "\n" (zipWith (\l d -> intercalate "|" [l, d]) ls diag)
+  where
+    ls = lines strng :: [String]
+    diag = diag1SymLines strng :: [String]
+
+oper :: (String -> String) -> String -> String
+oper fct = fct
+
+diag1SymLines :: [Char] -> [String]
+diag1SymLines strng = map (\x -> map (!! x) ls) [0 .. (n - 1)]
+  where
+    ls = lines strng :: [String]
+    n = length ls :: Int
+
+-- #againwhatlearned
+-- use `transpose` from Data.List
+-- Input: transpose ["ABCD","abcd"]
+-- Output: ["Aa","Bb","Cc","Dd"]
---
+++ test/Codewars/G964/Opstrings1Spec.hs
@@ -0,0 +1,38 @@
+module Codewars.G964.Opstrings1Spec where
+
+import Codewars.G964.Opstrings1
+import Test.Hspec
+import Test.QuickCheck
+import Text.Printf (printf)
+
+testOpstring4 :: String -> String -> Spec
+testOpstring4 r s =
+  it (printf "should return rot90Clock for strng: %s \n" (show r)) $
+    oper rot90Clock r `shouldBe` s
+
+testOpstring6 :: String -> String -> Spec
+testOpstring6 r s =
+  it (printf "should return diag1Sym for strng: %s \n" (show r)) $
+    oper diag1Sym r `shouldBe` s
+
+testOpstring8 :: String -> String -> Spec
+testOpstring8 r s =
+  it (printf "should return selfieAndDiag1 for strng: %s \n" (show r)) $
+    oper selfieAndDiag1 r `shouldBe` s
+
+spec :: Spec
+spec = do
+  describe "rot90Clock" $ do
+    testOpstring4
+      "rgavce\nvGcEKl\ndChZVW\nxNWgXR\niJBYDO\nSdmEKb"
+      "Sixdvr\ndJNCGg\nmBWhca\nEYgZEv\nKDXVKc\nbORWle"
+
+  describe "diag1Sym" $ do
+    testOpstring6
+      "wuUyPC\neNHWxw\nehifmi\ntBTlFI\nvWNpdv\nIFkGjZ"
+      "weetvI\nuNhBWF\nUHiTNk\nyWflpG\nPxmFdj\nCwiIvZ"
+
+  describe "selfieAndDiag1" $ do
+    testOpstring8
+      "NJVGhr\nMObsvw\ntPhCtl\nsoEnhi\nrtQRLK\nzjliWg"
+      "NJVGhr|NMtsrz\nMObsvw|JOPotj\ntPhCtl|VbhEQl\nsoEnhi|GsCnRi\nrtQRLK|hvthLW\nzjliWg|rwliKg"
