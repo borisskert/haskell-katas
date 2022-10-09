593f6e21165587838b46a2b5ac632d99ec806843
Boris Skert <boris.skert@gmail.com>
2022-10-09 08:21:16 +0200

    Solve 'Casino chips' kata

---
+++ src/Kata.hs
@@ -0,0 +1,12 @@
+module Kata (solve) where
+
+--  https://www.codewars.com/kata/5e0b72d2d772160011133654/train/haskell
+
+import Data.List (sort)
+
+solve :: [Int] -> Int
+solve abc
+  | x + y < z = x + y
+  | otherwise = (x + y - z) `div` 2 + z
+  where
+    (x : y : z : _) = sort abc
---
+++ test/KataSpec.hs
@@ -0,0 +1,18 @@
+module KataSpec (spec) where
+
+import Kata (solve)
+import Test.Hspec
+
+spec :: Spec
+spec = do
+  it "Basic tests" $ do
+    solve [1, 1, 1] `shouldBe` 1
+    solve [1, 2, 1] `shouldBe` 2
+    solve [4, 1, 1] `shouldBe` 2
+    solve [4, 5, 8] `shouldBe` 8
+    solve [8, 2, 8] `shouldBe` 9
+    solve [8, 1, 4] `shouldBe` 5
+    solve [7, 4, 10] `shouldBe` 10
+    solve [12, 12, 12] `shouldBe` 18
+    solve [1, 23, 2] `shouldBe` 3
+    solve [283444, 679735, 171258] `shouldBe` 454702
