fe003e301740824b6bc4ba16f0571f153c84b990
Boris Skert <boris.skert@gmail.com>
2022-10-13 17:40:43 +0200

    Solve 'Area of an arrow' kata

---
+++ src/Kata.hs
@@ -0,0 +1,9 @@
+module Kata (arrowArea) where
+
+-- https://www.codewars.com/kata/589478160c0f8a40870000bc/train/haskell
+
+arrowArea :: Int -> Int -> Double
+arrowArea a b = b' / 4 * a'
+  where
+    b' = fromIntegral b
+    a' = fromIntegral a
---
+++ test/KataSpec.hs
@@ -0,0 +1,12 @@
+module KataSpec (spec) where
+
+import Kata (arrowArea)
+import Test.Hspec
+import Test.QuickCheck
+
+spec :: Spec
+spec = do
+  it "Fixed tests" $ do
+    arrowArea 4 2 `shouldBe` 2.0
+    arrowArea 7 6 `shouldBe` 10.5
+    arrowArea 25 25 `shouldBe` 156.25
