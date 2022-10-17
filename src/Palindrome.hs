1867a8d8e4ea1a197c7e73ad5f71118ce2046567
Boris Skert <boris.skert@gmail.com>
2022-10-17 17:43:49 +0200

    Solve 'Numerical Palindrome #1.5' kata

---
+++ src/Palindrome.hs
@@ -0,0 +1,20 @@
+module Palindrome (palindrome) where
+
+--  https://www.codewars.com/kata/58e09234ca6895c7b300008c/train/haskell
+
+palindrome :: Int -> Int -> Maybe [Int]
+palindrome num s
+  | num < 0 || s < 0 = Nothing
+  | otherwise = Just . take s . filter isPalindrome $ [num ..]
+
+isPalindrome :: Int -> Bool
+isPalindrome x
+  | x > 9 = isPalindromic . show $ x
+  | otherwise = False
+
+isPalindromic :: (Eq a) => [a] -> Bool
+isPalindromic [] = True
+isPalindromic [_] = True
+isPalindromic xs
+  | head xs == last xs = isPalindromic . tail . init $ xs
+  | otherwise = False
