f96013d3f45bd6b702ef9df9a4ae38dde4a099e9
Boris Skert <boris.skert@gmail.com>
2023-01-14 09:53:57 +0100

    Solve 'Quarter of the year' kata

---
+++ src/Kata.hs
@@ -0,0 +1,6 @@
+module Kata (quarterOf) where
+
+-- https://www.codewars.com/kata/5ce9c1000bab0b001134f5af/train/haskell
+
+quarterOf :: Int -> Int
+quarterOf month = ((month - 1) `div` 3) + 1
---
+++ test/KataSpec.hs
@@ -0,0 +1,14 @@
+module KataSpec (spec) where
+
+import Test.Hspec
+import Test.QuickCheck
+import Kata (quarterOf)
+
+spec :: Spec
+spec = do
+  it "Basic tests" $ do
+    quarterOf 1  `shouldBe` 1
+    quarterOf 3  `shouldBe` 1
+    quarterOf 4  `shouldBe` 2
+    quarterOf 8  `shouldBe` 3
+    quarterOf 11 `shouldBe` 4
