4274533236548dbb760f7222ad914388e08ae43d
Boris Skert <boris.skert@gmail.com>
2023-02-03 08:44:30 +0100

    Solve 'Begin your day with a challenge, but an easy one.' kata

---
+++ src/Kata.hs
@@ -0,0 +1,20 @@
+module Kata (oneTwoThree) where
+
+-- https://www.codewars.com/kata/5873b2010565844b9100026d/train/haskell
+
+oneTwoThree :: Integral n => n -> [n]
+oneTwoThree 0 = [0, 0]
+oneTwoThree n = [divMod' 9 n, replicate' n 1]
+
+replicate' :: (Integral n, Integral a) => n -> a -> a
+replicate' 0 _ = 0
+replicate' 1 x = x
+replicate' n x = x + 10 * replicate' (n - 1) x
+
+divMod' :: (Integral n) => n -> n -> n
+divMod' n 0 = 0
+divMod' n x
+  | remainder > 0 = remainder + 10 * divMod' n (quotient * n)
+  | otherwise = n + 10 * divMod' n ((quotient - 1) * n)
+  where
+    (quotient, remainder) = x `divMod` n
---
+++ test/KataSpec.hs
@@ -0,0 +1,14 @@
+module KataSpec (spec, main) where
+import Kata (oneTwoThree)
+import Test.Hspec
+
+main :: IO ()
+main = hspec spec
+spec = do
+  describe "Example Tests" $
+    it "should work with example tests" $ do
+        oneTwoThree 0  `shouldBe` [0, 0]
+        oneTwoThree 1  `shouldBe` [1, 1]
+        oneTwoThree 2  `shouldBe` [2, 11]
+        oneTwoThree 3  `shouldBe` [3, 111]
+        oneTwoThree 19 `shouldBe` [991, 1111111111111111111]
