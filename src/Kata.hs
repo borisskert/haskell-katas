ec457159410be5cfd208caec9ba6c4c0e5fa7cd3
Boris Skert <boris.skert@gmail.com>
2023-02-12 09:30:21 +0100

    Solve 'Estimating Amounts of Subsets' kata

---
+++ src/Kata.hs
@@ -0,0 +1,8 @@
+module Kata (estSubsets) where
+
+-- https://www.codewars.com/kata/584703d76f6cf6ffc6000275/train/haskell
+
+import Data.List (nub)
+
+estSubsets :: Ord a => [a] -> Integer
+estSubsets = subtract 1 . (2 ^) . length . nub
---
+++ test/KataSpec.hs
@@ -0,0 +1,14 @@
+module KataSpec where
+import Test.Hspec
+import Kata (estSubsets)
+
+spec :: Spec
+spec = do
+  describe "Example Tests" $
+    it "should work with example tests" $ do
+      estSubsets [1, 2, 3, 4] `shouldBe` 15
+      estSubsets ['a', 'b', 'c', 'd', 'd'] `shouldBe` 15
+      estSubsets [2, 3, 4, 5, 5, 6, 6, 7, 8, 8] `shouldBe` 127
+      estSubsets ['a', 'z', 'z', 'z', 'b', 'j', 'f', 'k', 'b', 'd', 'j', 'j', 'n', 'm', 'm'] `shouldBe` 511
+      estSubsets [1, 1, 1, 1, 1, 1, 1, 1] `shouldBe` 1
+      estSubsets ([] :: [Integer])  `shouldBe` 0   
