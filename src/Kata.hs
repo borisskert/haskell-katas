1dce1247d3477af4a75ed5813e547ac59fcfcf91
Boris Skert <boris.skert@gmail.com>
2023-02-17 07:55:01 +0100

    Solve 'Sortable Shapes' kata

---
+++ src/Kata.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE InstanceSigs #-}
+
+module Kata (Shape (..)) where
+
+-- https://www.codewars.com/kata/586669a8442e3fc307000048/train/haskell
+
+data Shape
+  = CustomShape {area :: Double}
+  | Square {side :: Double}
+  | Circle {radius :: Double}
+  | Triangle {base :: Double, height :: Double}
+  | Rectangle {width :: Double, height :: Double}
+  deriving (Show, Eq)
+
+instance Ord Shape where
+  (<=) :: Shape -> Shape -> Bool
+  (<=) a b = areaOf a <= areaOf b
+
+areaOf :: Shape -> Double
+areaOf (Square side) = side * side
+areaOf (Circle radius) = radius * radius * pi
+areaOf (Triangle a b) = 0.5 * a * b
+areaOf (Rectangle a b) = a * b
+areaOf (CustomShape a) = a
---
+++ test/KataSpec.hs
@@ -0,0 +1,32 @@
+module KataSpec where
+import Kata (Shape (..))
+import Data.List (sort)
+import Test.QuickCheck
+import Test.Hspec
+
+shapes :: [Shape]
+shapes = [
+    CustomShape area,
+    Square side,
+    Circle radius,
+    Triangle triangleBase1 triangleHeight1,
+    Triangle triangleBase2 triangleHeight2,
+    Rectangle width height,
+    CustomShape area2]
+    where
+      area = 1.1234
+      side = 1.1234
+      radius = 1.1234
+      triangleBase1 = 2.0
+      triangleHeight1 = 5.0
+      triangleBase2 = 3.0
+      triangleHeight2 = 4.0
+      width = 4
+      height = 4
+      area2 = 16.1
+      
+spec :: Spec
+spec = do
+  describe "Example Tests" $
+    it "should work with example tests" $
+        forAll (shuffle shapes) (\ss -> sort shapes == shapes)
