ef5037f63abfb60b8801bdb9f4bca2846e061f7e
Boris Skert <boris.skert@gmail.com>
2023-09-29 08:33:46 +0200

    Solve 'Calculate Pyramid Height' kata

---
+++ src/Kata.hs
@@ -0,0 +1,11 @@
+module Kata (pyramidHeight) where
+
+-- https://www.codewars.com/kata/56968ce7753513604b000055/train/haskell
+
+pyramidHeight :: Int -> Int
+pyramidHeight x =
+  length
+    . takeWhile (<= x)
+    . tail
+    . scanl (\sum x -> sum + x * x) 0
+    $ [1 ..]
---
+++ test/KataSpec.hs
@@ -0,0 +1,26 @@
+module KataSpec (spec) where
+
+import Test.Hspec
+import Test.QuickCheck
+import Kata (pyramidHeight)
+
+spec :: Spec
+spec = do
+  it "Basic tests" $ do
+    pyramidHeight 1  `shouldBe` 1
+    pyramidHeight 4  `shouldBe` 1
+    pyramidHeight 5  `shouldBe` 2
+    pyramidHeight 29  `shouldBe` 3
+    pyramidHeight 30  `shouldBe` 4
+    pyramidHeight 31  `shouldBe` 4
+    pyramidHeight 1239  `shouldBe` 14
+    pyramidHeight 1240  `shouldBe` 15
+    pyramidHeight 1241  `shouldBe` 15
+    pyramidHeight 1495  `shouldBe` 15
+    pyramidHeight 1496  `shouldBe` 16
+    pyramidHeight 2838  `shouldBe` 19
+    pyramidHeight 4324  `shouldBe` 23
+    pyramidHeight 4349  `shouldBe` 23
+    pyramidHeight 4517  `shouldBe` 23
+    pyramidHeight 6169  `shouldBe` 25
+    pyramidHeight 9129  `shouldBe` 29
