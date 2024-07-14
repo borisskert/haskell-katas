6e991d1e86c6427b09c440856ee9f3ad3eea6a36
Boris Skert <boris.skert@gmail.com>
2024-07-14 14:48:55 +0200

    Clean up

--- src/Haskell/SylarDoom/Smallfuck.hs
+++ src/Haskell/SylarDoom/Smallfuck.hs
@@ -1,44 +1,55 @@
-module Haskell.SylarDoom.Smallfuck (interpreter, unreadOne, jumpBackward, jumpForward) where
+module Haskell.SylarDoom.Smallfuck (interpreter) where
 
 --  https://www.codewars.com/kata/58678d29dbca9a68d80000d7/train/haskell
 
+import Data.Function (on)
+
 interpreter :: String -> String -> String
-interpreter code tape = process ([], code) ([], tape)
+interpreter = process `on` read'
 
 type Tape = (String, String)
 
 process :: Tape -> Tape -> String
-process (_, []) (read, unread) = read ++ unread
-process code@(_, '*' : cs) (read, '0' : ts) = process (readOne code) (read, '1' : ts)
-process code@(_, '*' : cs) (read, '1' : ts) = process (readOne code) (read, '0' : ts)
-process code@(_, '>' : cs) (read, [current]) = read ++ [current]
-process code@(_, '>' : cs) tape@(read, t : ts) = process (readOne code) (readOne tape)
-process code@(_, '<' : cs) ([], unread) = unread
-process code@(_, '<' : cs) tape@(read, unread) = process (readOne code) (unreadOne tape)
-process code@(_, '[' : cs) tape@(read, '0' : unread) = process (jumpForward code) tape
-process code@(_, ']' : cs) tape@(read, '1' : unread) = process (jumpBackward code) tape
-process code@(_, _ : cs) (read, unread) = process (readOne code) (read, unread)
+process (_, []) tape = show' tape
+process (_, '>' : _) tape@(_, [_]) = show' tape
+process code@(_, '>' : _) tape = process (moveRight code) (moveRight tape)
+process (_, '<' : _) ([], unread) = unread
+process code@(_, '<' : _) tape = process (moveRight code) (moveLeft tape)
+process code@(_, '[' : _) tape@(_, '0' : _) = process (jumpForward code) tape
+process code@(_, ']' : _) tape@(_, '1' : _) = process (jumpBackward code) tape
+process code@(_, '*' : _) tape = process (moveRight code) (flipBit tape)
+process code@(_, _ : _) tape = process (moveRight code) tape
 
-readOne :: Tape -> Tape
-readOne (read, current : unread) = (read ++ [current], unread)
+read' :: String -> Tape
+read' xs = ([], xs)
 
-unreadOne :: Tape -> Tape
-unreadOne (read, unread) = (init read, last read : unread)
+show' :: Tape -> String
+show' (read, unread) = read ++ unread
+
+flipBit :: Tape -> Tape
+flipBit (read, '0' : ts) = (read, '1' : ts)
+flipBit (read, '1' : ts) = (read, '0' : ts)
+
+moveRight :: Tape -> Tape
+moveRight (read, current : unread) = (read ++ [current], unread)
+
+moveLeft :: Tape -> Tape
+moveLeft (read, unread) = (init read, last read : unread)
 
 jumpForward :: Tape -> Tape
-jumpForward tape = go 0 (readOne tape)
+jumpForward tape = go 0 (moveRight tape)
   where
     go :: Int -> Tape -> Tape
-    go 0 tape@(read, ']' : unread) = tape
-    go n tape@(read, ']' : unread) = go (n - 1) (readOne tape)
-    go n tape@(read, '[' : unread) = go (n + 1) (readOne tape)
-    go n tape@(read, _ : unread) = go n (readOne tape)
+    go 0 tape@(_, ']' : _) = tape
+    go n tape@(_, ']' : _) = go (n - 1) (moveRight tape)
+    go n tape@(_, '[' : _) = go (n + 1) (moveRight tape)
+    go n tape = go n (moveRight tape)
 
 jumpBackward :: Tape -> Tape
-jumpBackward tape = go 0 (unreadOne tape)
+jumpBackward tape = go 0 (moveLeft tape)
   where
     go :: Int -> Tape -> Tape
-    go 0 tape@(read, '[' : unread) = tape
-    go n tape@(read, '[' : unread) = go (n - 1) (unreadOne tape)
-    go n tape@(read, ']' : unread) = go (n + 1) (unreadOne tape)
-    go n tape@(read, _ : unread) = go n (unreadOne tape)
+    go 0 tape@(_, '[' : _) = tape
+    go n tape@(_, '[' : _) = go (n - 1) (moveLeft tape)
+    go n tape@(_, ']' : _) = go (n + 1) (moveLeft tape)
+    go n tape = go n (moveLeft tape)
