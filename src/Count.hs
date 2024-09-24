f7adb8d33eb44acda4a22573314585b8cefb2b66
Boris Skert <boris.skert@gmail.com>
2024-09-24 09:56:47 +0200

    Solve 'Count characters in your string' kata

---
+++ src/Count.hs
@@ -0,0 +1,14 @@
+module Count (count) where
+
+-- https://www.codewars.com/kata/52efefcbcdf57161d4000091/train/haskell
+
+import qualified Data.Map as Map
+
+count :: String -> [(Char, Int)]
+count = Map.toList . go Map.empty
+
+go :: Map.Map Char Int -> String -> Map.Map Char Int
+go = foldl (flip inc)
+
+inc :: Char -> Map.Map Char Int -> Map.Map Char Int
+inc = Map.alter (Just . maybe 1 (+1))
---
+++ test/CountSpec.hs
@@ -0,0 +1,15 @@
+module CountSpec where
+
+import Count (count)
+import Test.Hspec
+
+spec :: Spec
+spec = do
+  it "Fixed Tests" $ do
+    mapM_
+      (\(inp, ans) -> count inp `shouldMatchList` ans)
+      [ ("aba", [('a', 2), ('b', 1)]),
+        ("", []),
+        ("aa", [('a', 2)]),
+        ("aabb", [('a', 2), ('b', 2)])
+      ]
